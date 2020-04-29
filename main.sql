f9311fae6212ea75f26a2dc0ebd7dd07791907f3
Adilyam <adliyamt@gmail.com>
2020-04-29 11:33:41 +0600

    init commit

--- main.sql
+++ main.sql
@@ -4,20 +4,33 @@ SET AUTOCOMMIT ON;
 declare
     num number(10);
 begin
-    course_project.popular_subject(1, 2016);
+    course_project.popular_subject(1, 2017);
+    dbms_output.put_line('_________');
     course_project.popular_teacher(1, '01-APR-16 12:00:00 AM', 'ECO 101'); 
+    dbms_output.put_line('_________');
     course_project.calculate_gpa(1, '01-APR-16 12:00:00 AM'); 
+    dbms_output.put_line('_________');
     course_project.zero_reg(1, '01-APR-16 12:00:00 AM'); 
-    course_project.retake_fee(1, '01-APR-16 12:00:00 AM'); 
+    dbms_output.put_line('_________');
+    course_project.retake_fee(1, '01-APR-16 12:00:00 AM');
+    dbms_output.put_line('_________');
     course_project.calculate_loading('01-APR-16 12:00:00 AM'); 
+    dbms_output.put_line('_________');
     course_project.stud_schedule('01-APR-16 12:00:00 AM', '004531EC911B9D3EF9A7C7A8524D054EA943BA3F', 1);
-    course_project.teacher_schedule('01-APR-16 12:00:00 AM');
+    dbms_output.put_line('_________');
+    course_project.teacher_schedule('01-APR-16 12:00:00 AM', '10081');
+    dbms_output.put_line('_________');
     course_project.count_subjects;
+    dbms_output.put_line('_________');
     course_project.student_flow;
+    dbms_output.put_line('_________');
     course_project.teacher_rating;
+    dbms_output.put_line('_________');
     course_project.course_rating;
+    dbms_output.put_line('_________');
     num := course_project.retake_fee;
 end;
+
 
 CREATE OR REPLACE TRIGGER grade_changes 
 BEFORE DELETE OR INSERT OR UPDATE ON course_selections 
@@ -36,4 +49,3 @@ update  course_selections
 set QIYMET_YUZ = 84
 where ders_kod = 'LAW 212' and stud_id = '004531EC911B9D3EF9A7C7A8524D054EA943BA3F'
 
-TYPE Calendar IS VARRAY(366) OF DATE;
